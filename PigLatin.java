37f7735289b1d855f7c83f58954d6d732b629983
Faria-Lorin <florin20@stuy.edu>
2021-01-04 21:30:49 -0500

    I added a pigLatin method and used code from pigLatinSimple for when input words start with a vowel

--- PigLatin.java
+++ PigLatin.java
@@ -12,8 +12,17 @@ public class PigLatin {
       }
       out += s.charAt(0) + "ay";
     }
-      return out;
-    }
+    return out;
+  }
+  public static String pigLatin(String s){
+    String out = "";
+    if (s.charAt(0) == 'a' || s.charAt(0) == 'e' || s.charAt(0) == 'i' || s.charAt(0) == 'o' || s.charAt(0) == 'u'){
+        out = s;
+        out += "hay";
+      }
+    return out;
+  }
+  
   public static void main(String[] args) {
     System.out.println(pigLatinSimple(args[0]));
   }
