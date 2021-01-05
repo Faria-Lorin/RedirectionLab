a9bb8320b33217444324d93036d9311feb570b3e
Faria-Lorin <florin20@stuy.edu>
2021-01-04 21:22:26 -0500

    I created the pigLatinSimple method and added code so that when the first letter of the inputed word is a vowel, the output string is the input string with hay tacked on at the end and I added a main method to easily test input strings

--- PigLatin.java
+++ PigLatin.java
@@ -2,6 +2,13 @@ import java.util.*;
 public class PigLatin {
   public static String pigLatinSimple(String s){
     String out = "";
-    return out;
+    if (s.charAt(0) == 'a' || s.charAt(0) == 'e' || s.charAt(0) == 'i' || s.charAt(0) == 'o' || s.charAt(0) == 'u'){
+        out = s;
+        out += "hay";
+      }
+      return out;
+    }
+  public static void main(String[] args) {
+    System.out.println(pigLatinSimple(args[0]));
   }
 }
